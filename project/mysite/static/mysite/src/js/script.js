c8051cec4705c7eae8a59d257546be90cfc92b04
samatncs <samatncs@gmail.com>
2023-12-27 16:50:43 +0300

    added new app:mysite, changed and moved index.html to folder templates

---
+++ project/mysite/static/mysite/src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 1500,
+      autoplayHoverPause: true,
+    });
+  });
